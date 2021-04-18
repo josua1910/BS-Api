dcbf99ecc59ea8f5d742d1e01bc0506e83decdea
Rey <82385174+reyhangans@users.noreply.github.com>
2021-04-18 15:30:54 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -19,6 +19,34 @@ router.get('/api/news', (req, res) => {
     res.redirect('https://raw.githubusercontent.com/Zhirrr/My-SQL-Results/main/Berita.json')
 })
 
+router.get('api/cekapikey', (req, res) => {
+    config = {
+        status: true,
+        result: {
+            prefix : '/',
+            namabot: 'ZhirrrBot',
+            namaowner: 'Zhirrr',
+            instagram: 'zhirr_ajalah',
+            youtube : 'Gak Punya'
+        }
+    }
+    res.json(config)
+})
+
+router.get('/cekapikey', (req, res) => {
+    config = {
+        status: true,
+        result: {
+            prefix : '/',
+            namabot: 'ZhirrrBot',
+            namaowner: 'Zhirrr',
+            instagram: 'zhirr_ajalah',
+            youtube : 'Gak Punya'
+        }
+    }
+    res.json(config)
+})
+
 router.get('/config', (req, res) => {
     config = {
         status: true,
